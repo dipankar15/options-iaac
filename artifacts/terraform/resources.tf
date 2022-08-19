997a036f6a9a89ec4547102734a18541ac87f1e1
dipankar15 <chatterjee.dipankar@gmail.com>
2022-08-19 19:32:04 +0530

    Add files via upload

---
+++ artifacts/terraform/resources.tf
@@ -0,0 +1,42 @@
+
+resource "aws_instance" "options-vm" {
+  count = "${var.instance_count}"
+
+  ami           = "${var.ami}"
+  instance_type = "${var.instance}"
+  key_name      = "${var.private_key}"
+
+  vpc_security_group_ids = [
+    "${var.secgroupname}"
+  ]
+
+
+  ebs_block_device {
+    device_name           = "/dev/sdg"
+    volume_size           = 20
+    volume_type           = "gp2"
+    encrypted             = false
+    delete_on_termination = true
+  }
+
+  connection {
+    private_key = "${file(var.private_key_file)}"
+    user        = "${var.ansible_user}"
+    host = self.public_ip
+  }
+
+  # Ansible requires Python to be installed on the remote machine as well as the local machine.
+  provisioner "remote-exec" {
+    inline = ["sudo apt update -y","sudo apt-get install python3 -y"]
+  }
+
+  provisioner "local-exec" {
+    command = "ANSIBLE_HOST_KEY_CHECKING=False  ansible-playbook -u ${var.ansible_user} --private-key /home/dipankar/${var.private_key}.pem -i ${aws_instance.options-vm.0.public_ip}, ../playbooks/install_modules.yaml"
+  }
+
+ 
+  tags = {
+    Name     = "options-scanner-${count.index +1 }"
+  }
+}
+
---
+++ artifacts/terraform/variables.tf
@@ -0,0 +1,32 @@
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "instance" {
+  default = "t2.micro"
+}
+
+variable "instance_count" {
+  default = "1"
+}
+
+
+variable "private_key" {
+  default = "options-key"
+}
+variable "private_key_file" {
+  default = "/home/dipankar/options-key.pem"
+}
+
+variable "ansible_user" {
+  default = "ubuntu"
+}
+
+variable "secgroupname" {
+    default = "sg-bc687cc5"
+}
+
+variable "ami" {
+  default = "ami-0d70546e43a941d70"
+}
